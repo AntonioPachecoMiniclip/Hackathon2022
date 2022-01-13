ce777341534a5f19ebd635474112b0b0ff3f6891
Teresa Gomes <teresa.gomes@miniclip.com>
2022-01-13 23:20:28 +0000

    - Added Exit Button to Credits Scene - Updated assets on Main Menu

--- Assets/Scripts/ExitInput.cs
+++ Assets/Scripts/ExitInput.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsInput : MonoBehaviour
+public class ExitInput : MonoBehaviour
 {
 
     public void exitButtonCallback() {
