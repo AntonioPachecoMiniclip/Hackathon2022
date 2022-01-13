af4a9707697fc0b59412a5688d8b32f58a172a80
António Pacheco <antonio.pacheco@miniclip.com>
2022-01-13 17:50:50 +0000

    Added caps materials

---
+++ Assets/Scripts/Soundbank.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundbank : MonoBehaviour
+{
+    public AudioClip playMetalCap;
+    public AudioClip playPlasticCap;
+    public AudioClip cheering;
+}
