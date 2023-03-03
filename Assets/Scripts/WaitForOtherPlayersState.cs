20e77898a7c152180cafbeb3e3e23826655b062b
Gabriel Veiga <gabriel.veiga@miniclip.com>
2023-03-03 23:03:43 +0000

    Checkmark on turn done

--- Assets/Scripts/WaitForOtherPlayersState.cs
+++ Assets/Scripts/WaitForOtherPlayersState.cs
@@ -2,17 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaitForOtherPlayersState : GameState
 {
+    GameObject check;
+
     public override void OnEnter()
     {
         base.OnEnter();
         Debug.Log("Waiting For Other Players");
+
+        if (check == null)
+            check = GameObject.FindGameObjectWithTag("Checkmark");
+
+        check.GetComponent<Image>().enabled = true;
     }
 
     protected override void TriggerEndState()
     {
+        if (check == null)
+            check = GameObject.FindGameObjectWithTag("Checkmark");
+        check.GetComponent<Image>().enabled = false;
+
         gameStateMachine.SetState<PlayTurnsState>();
     }
 
